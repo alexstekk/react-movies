fd9f52af9249a4955dcaf13837f86ac8023da9e1
Alexander Stekolshchikov <alexstekk@yandex.ru>
2022-09-22 14:52:39 +0300

    added filter

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -1,20 +1,35 @@
 import React, { Component } from 'react';
 import { Movies } from '../components/Movies';
 import { Preloader } from '../components/Preloader';
+import { Search } from '../components/Search';
 
 class Main extends Component {
 	state = {
 		movies: [],
+		loading: true,
+	};
+
+	searchMovies = (str, type = 'all') => {
+		this.setState({ loading: true });
+		fetch(`http://www.omdbapi.com/?apikey=6c6f4eae&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+			.then((response) => response.json())
+			.then((data) => this.setState({ movies: data.Search, loading: false }));
 	};
 
 	componentDidMount() {
 		fetch('http://www.omdbapi.com/?apikey=6c6f4eae&s=matrix')
 			.then((response) => response.json())
-			.then((data) => this.setState({ movies: data.Search }));
+			.then((data) => this.setState({ movies: data.Search, loading: false }));
 	}
 	render() {
-		const { movies } = this.state;
-		return <main className='container content'>{movies.length ? <Movies movies={this.state.movies} /> : <Preloader />}</main>;
+		const { movies, loading } = this.state;
+
+		return (
+			<main className='container content'>
+				<Search searchMovies={this.searchMovies} />
+				{loading ? <Preloader /> : <Movies movies={movies} />}
+			</main>
+		);
 	}
 }
 
