00009cb59f05bb1a5033e2a05ccbae2e51fdc830
Alexander Stekolshchikov <alexstekk@yandex.ru>
2022-09-28 17:48:33 +0300

    added https to fetch

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -13,15 +13,19 @@ class Main extends Component {
 
 	searchMovies = (str, type = 'all') => {
 		this.setState({ loading: true });
-		fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+		fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
 			.then((response) => response.json())
 			.then((data) => this.setState({ movies: data.Search, loading: false }));
 	};
 
 	componentDidMount() {
-		fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
+		fetch(`https://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
 			.then((response) => response.json())
-			.then((data) => this.setState({ movies: data.Search, loading: false }));
+			.then((data) => this.setState({ movies: data.Search, loading: false }))
+			.catch((err) => {
+				console.error(err);
+				this.setState({ loading: false });
+			});
 	}
 	render() {
 		const { movies, loading } = this.state;
