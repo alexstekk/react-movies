d3d210ef68087bdb1afa919d1c90d15697ca4ddd
Alexander Stekolshchikov <alexstekk@yandex.ru>
2022-09-28 17:25:26 +0300

    complete project

--- src/layout/Main.jsx
+++ src/layout/Main.jsx
@@ -3,6 +3,8 @@ import { Movies } from '../components/Movies';
 import { Preloader } from '../components/Preloader';
 import { Search } from '../components/Search';
 
+const API_KEY = process.env.REACT_APP_API_KEY;
+
 class Main extends Component {
 	state = {
 		movies: [],
@@ -11,13 +13,13 @@ class Main extends Component {
 
 	searchMovies = (str, type = 'all') => {
 		this.setState({ loading: true });
-		fetch(`http://www.omdbapi.com/?apikey=6c6f4eae&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
+		fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=${str}${type !== 'all' ? `&type=${type}` : ''}`)
 			.then((response) => response.json())
 			.then((data) => this.setState({ movies: data.Search, loading: false }));
 	};
 
 	componentDidMount() {
-		fetch('http://www.omdbapi.com/?apikey=6c6f4eae&s=matrix')
+		fetch(`http://www.omdbapi.com/?apikey=${API_KEY}&s=matrix`)
 			.then((response) => response.json())
 			.then((data) => this.setState({ movies: data.Search, loading: false }));
 	}
